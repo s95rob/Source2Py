a991e5073c411b420b73a946afe1b9f2c86805c1
s95rob <s95rob@gmail.com>
2023-10-20 11:50:00 -0700

    cmake: find python using FindPython3

--- CMake/Python.cmake
+++ CMake/Python.cmake
@@ -1,23 +1,9 @@
-# Check if Python vars have been set in CLI
+# Check if Python environment variables have been set
 
-message(STATUS "Checking if Python paths have been set...")
+find_package(Python3 COMPONENTS Interpreter Development)
 
-if (NOT DEFINED PYTHON_INCLUDE_DIR)
-	message(FATAL_ERROR "Please provide Python include directory as CMake argument! Usage: -DPYTHON_INCLUDE_DIR=/path/to/python/include")
+if (NOT DEFINED Python3_Development_FOUND)
+	message(FATAL_ERROR "Please make sure Python3 (including development artifacts) installed correctly on your system!")
 else()
-	message(STATUS "PYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR}")
+	message(STATUS "Linking Python ${Python3_VERSION}")
 endif()
-
-if (NOT DEFINED PYTHON_LIB_DIR)
-	message(FATAL_ERROR "Please provide Python lib directory as CMake argument! Usage: -DPYTHON_LIB_DIR=/path/to/python/lib")
-else()
-	message(STATUS "PYTHON_LIB_DIR=${PYTHON_LIB_DIR}")
-endif()
-
-if (NOT DEFINED PYTHON_LIB)
-	message(FATAL_ERROR "Please provide Python library as CMake argument! Usage -DPYTHON_LIB=python3.11")
-else()
-	message(STATUS "PYTHON_LIB=${PYTHON_LIB}")
-endif()
-
-message(STATUS "Python OK!")
